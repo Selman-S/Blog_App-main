bccfbc577de631f96978752d1385b1ecb2cdd190
selman <gunduzgece546@gmail.com>
2022-10-14 14:27:07 +0300

    blog app django

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from blog.models import BlogPost, Category, Comment, Like, Post_view
+
+admin.site.register(Category)
+admin.site.register(Like)
+admin.site.register(BlogPost)
+admin.site.register(Comment)
+admin.site.register(Post_view)
